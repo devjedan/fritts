1fcd0e5f1a532c9238ecba562fa1a9f38b6e3330
devjedan <143809189+devjedan@users.noreply.github.com>
2023-09-04 20:02:35 +0530

    Add files via upload

---
+++ precache-manifest.e9480a6ff60a9e38cc8e816ee24b6cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5bdc1dd4642b597933071ba8f5982cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "babedd8253b5f879376a",
+    "url": "/static/css/main.2b830ea3.chunk.css"
+  },
+  {
+    "revision": "7c514edb19a6de1a417b",
+    "url": "/static/js/2.c1511b7c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c1511b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "babedd8253b5f879376a",
+    "url": "/static/js/main.0ff4fc31.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);
